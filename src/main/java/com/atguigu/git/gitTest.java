0c07ebc8bf81274f7b12f564f1aa001c5b2945c5
Pancy <49429758+yuanxiaopan@users.noreply.github.com>
2022-02-13 19:29:15 +0800

    Update gitTest.java

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -6,6 +6,6 @@ public class gitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
-        System.out.println("hello git5!");
+        System.out.println("hello git6!");
     }
 }
